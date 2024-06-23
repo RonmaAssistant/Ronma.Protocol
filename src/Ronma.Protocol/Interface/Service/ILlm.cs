09914f284c163164f3ea34bacd4f76124a9049be
YvesShen <KevinYeti@gmail.com>
2024-06-24 06:49:48 +0800

    intro LLM interface

---
+++ src/Ronma.Protocol/Interface/Service/ILlm.cs
@@ -0,0 +1,11 @@
+﻿using Ronma.Protocol.Structure;
+
+namespace Ronma.Protocol.Interface.Service
+{
+    public interface ILlm : IService
+    {
+        string Name { get; }
+        string Prompt { get; }
+        EndpointInfo Endpoint { get; }
+    }
+}
