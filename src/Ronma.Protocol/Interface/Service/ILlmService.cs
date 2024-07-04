ed84baf83965dc5e8deb12b69764346d3371bbc0
YvesShen <KevinYeti@gmail.com>
2024-07-05 06:51:52 +0800

    rename LlmContentFile to LlmContentPayload

--- src/Ronma.Protocol/Interface/Service/ILlmService.cs
+++ src/Ronma.Protocol/Interface/Service/ILlmService.cs
@@ -4,10 +4,8 @@ namespace Ronma.Protocol.Interface.Service
 {
     public interface ILlmService : IService
     {
-        string Name { get; set; }
-        string Prompt { get; set; }
         LlmEndpoint Endpoint { get; set; }
 
-        LlmContent Perform(LlmContent message);
+        LlmContent Perform(string prompt, LlmContent message);
     }
 }
---
+++ src/Ronma.Protocol/Structure/LlmContentPayload.cs
@@ -0,0 +1,12 @@
+﻿using Ronma.Protocol.Enums;
+
+namespace Ronma.Protocol.Structure
+{
+    public class LlmContentPayload
+    {
+        public LlmContentPayloadType Type { get; set; }
+
+        public byte[] Content { get; set; }
+
+    }
+}
