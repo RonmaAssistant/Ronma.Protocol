8d34ab9891ac485f96d1fa29af44a75657480d89
YvesShen <KevinYeti@gmail.com>
2024-07-27 06:40:03 +0800

    dev ServiceInfo

--- src/Ronma.Protocol/Structure/ServiceInfo.cs
+++ src/Ronma.Protocol/Structure/ServiceInfo.cs
@@ -4,6 +4,13 @@ namespace Ronma.Protocol.Structure
 {
     public record ServiceInfo
     {
+        public ServiceInfo(BusChannel channel, string service, string command, string desc)
+        { 
+            Channel = channel;
+            Service = service;
+            Command = command;
+            Desc = desc;
+        }
         public BusChannel Channel { get; init; }
 
         public string Service {  get; init; }
