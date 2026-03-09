15bd219a564e5ce1b7cf97a49820831ebf3fee88
YvesShen <KevinYeti@gmail.com>
2026-03-09 21:58:34 +0100

    0.1.6.2

--- src/Ronma.Protocol/Structure/ServiceInfo.cs
+++ src/Ronma.Protocol/Structure/ServiceInfo.cs
@@ -6,27 +6,41 @@ namespace Ronma.Protocol.Structure
     {
         public ServiceInfo()
         {
-
         }
+
         public ServiceInfo(BusChannel channel, string service)
+            : this(channel, service, service)
+        {
+        }
+
+        public ServiceInfo(BusChannel channel, string service, string routing)
         {
             Channel = channel;
             Service = service;
+            Routing = routing;
             Desc = string.Empty;
         }
 
         public ServiceInfo(BusChannel channel, string service, ServiceCommand[] commands, string desc)
+            : this(channel, service, service, commands, desc)
+        {
+        }
+
+        public ServiceInfo(BusChannel channel, string service, string routing, ServiceCommand[] commands, string desc)
         { 
             Channel = channel;
             Service = service;
+            Routing = routing;
             Commands = [.. commands];
             Desc = desc ?? string.Empty;
         }
         public BusChannel Channel { get; init; }
 
-        public string Service {  get; init; }
+        public string Service {  get; init; } = string.Empty;
 
-        public string Desc { get; init; }
+        public string Routing { get; init; } = string.Empty;
+
+        public string Desc { get; init; } = string.Empty;
 
         public List<ServiceCommand> Commands { get; init; } = new List<ServiceCommand>();
 
